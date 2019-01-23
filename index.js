11ee4250703653ec305fac2d387e16586b6f57d7
Pardis Rezaei <pardisrezaei@gmail.com>
2019-01-23 01:01:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution here!
 const drivers= [ "Milo", "Otis", "Garfield"]
 
-Function destructivelyAppendDriver(name)
+Function destructivelyAppendDriver(name){
 drivers.push(pishi)
+}
